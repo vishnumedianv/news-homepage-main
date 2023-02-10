1406f1480231f573b7a0c06c2710e9ef833a87e5
Vishnu Vasita <118429816+vishnumedianv@users.noreply.github.com>
2023-02-10 15:38:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function openmenu() {
+  document.getElementsByClassName("menu-box")[0].style.display = "block";
+}
+
+function closemenu() {
+  document.getElementsByClassName("menu-box")[0].style.display = "none";
+}
